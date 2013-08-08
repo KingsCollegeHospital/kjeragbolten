4d46efc5cf2ac2b80cc6ad4601a1d85597c4965a
Elijah Charles <elijah.charles@nhs.net>
2013-08-08 11:31:39 +0100

    Fixed POM and converter
    Should convert multipage fine now

--- src/main/scala/uk/kch/nhs/geneva/kjeragbolten/util/Pdf2Tiff.scala
+++ src/main/scala/uk/kch/nhs/geneva/kjeragbolten/util/Pdf2Tiff.scala
@@ -40,7 +40,7 @@ object Pdf2Tiff {
 		op.addImage("-"); // read from stdin
 		op.trim(); // trim transparent edges
 		op.background("white"); // some viewers have a default white background
-		op.flatten(); // flatten layers
+		op.alpha("off"); // flatten layers
 		op.compress("lzw"); // compress
 		op.`type`("palette"); // reduce colour space to save file size
 		op.addImage("tif:-"); // write to stdout in tif-format
